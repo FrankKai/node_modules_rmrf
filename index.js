1b92db660ab420be9074decda80af03890c32543
FrankKai <gaokai20100801@gmail.com>
2022-12-23 18:09:26 +0800

    npmrc

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const minimist = require("minimist");
 const { pathToRegexp } = require("path-to-regexp");
 const rimraf = require("rimraf");
 
-
 const argv = process.argv.slice(2);
 console.log("minimist argv", minimist(argv));
 const { dir, deep } = minimist(argv);
-console.log('dir', dir)
+console.log("dir", dir);
 const node_modules_dir = `${dir}/node_modules`;
-const regexp = pathToRegexp(node_modules_dir)
-console.log('regexp', regexp)
-rimraf(regexp)
+const regexp = pathToRegexp(node_modules_dir);
+console.log("regexp", regexp);
+rimraf(regexp, function (err) {
+  console.log(err);
+});
