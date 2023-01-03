7463e976ca5bba619ade0abf373c63d9bf477236
FrankKai <gaokai20100801@gmail.com>
2023-01-03 17:50:39 +0800

    fix: sync

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ const fs = require("fs");
 
 const node_modules_dir = `node_modules`;
 
-glob(`**/${node_modules_dir}`, {}, function (er, files) {
+glob(`**/${node_modules_dir}/`, {}, function (er, files) {
   // files is an array of filenames.
   // If the `nonull` option is set, and nothing
   // was found, then files is ["**/*.js"]
   // er is an error object or null.
   console.log("files:::", files);
   for (const dir of files) {
-    console.log("dir:::", dir);
     if (fs.existsSync(dir)) {
       console.log("removed:::", dir);
-      rimraf(dir, {}, (err) => {
-        console.log("err:::", err);
-      });
+      rimraf.sync(dir)
     }
   }
 });
