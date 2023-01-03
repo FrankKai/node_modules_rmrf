3ab7d868c5de558bdff70546462a1edd5b39cdfc
FrankKai <gaokai20100801@gmail.com>
2023-01-03 10:59:28 +0800

    fix

--- index.js
+++ index.js
@@ -3,15 +3,25 @@
 const minimist = require("minimist");
 const { pathToRegexp } = require("path-to-regexp");
 const rimraf = require("rimraf");
+const rimraf = require("glob");
 
 const argv = process.argv.slice(2);
 console.log("minimist argv", minimist(argv));
 const { dir, deep } = minimist(argv);
 console.log("dir", dir);
-const node_modules_dir = `${dir}/node_modules`;
-const regexp = pathToRegexp(node_modules_dir);
-console.log("regexp", regexp);
-rimraf(node_modules_dir, function (err) {
-// rimraf(regexp, function (err) {
-  console.log(err);
+// const node_modules_dir = `${dir}/node_modules`;
+const node_modules_dir = `node_modules`;
+
+glob(`**/${node_modules_dir}`, options, function (er, files) {
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+  console.log('files:::', files)
 });
+// const regexp = pathToRegexp(node_modules_dir);
+// console.log("regexp", regexp);
+// rimraf(node_modules_dir, function (err) {
+//   // rimraf(regexp, function (err) {
+//   console.log(err);
+// });
