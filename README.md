fa235f3f163f5af0dd652477c159bc72b0508d35
FrankKai <gaokai20100801@gmail.com>
2023-01-03 13:40:21 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # node_modules_rmrf
 rm -rf specified dir's all node_modules, including all sub directory's node_modules recursively
+
+
+## cmd
+```
+node_modules_rmrf
+```
+
+## usage
+
+```
+.
+ |-parent
+ | |-chdir1
+ | | |-node_modules
+ | | |-file1.txt
+ | |-chdir2
+ | | |-node_modules
+ | | |-file2.txt
+ | | |-file3.sh
+```
+
+=>
+
+```
+.
+ |-parent
+ | |-chdir1
+ | | |-file1.txt
+ | |-chdir2
+ | | |-file2.txt
+ | | |-file3.sh
+```
--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 
-// const minimist = require("minimist");
-// const { pathToRegexp } = require("path-to-regexp");
 const rimraf = require("rimraf");
 const glob = require("glob");
 const fs = require("fs");
 
-// const argv = process.argv.slice(2);
-// console.log("minimist argv", minimist(argv));
-// const { dir, deep } = minimist(argv);
-// console.log("dir", dir);
-// const node_modules_dir = `${dir}/node_modules`;
 const node_modules_dir = `node_modules`;
 
 glob(`**/${node_modules_dir}`, {}, function (er, files) {
@@ -18,17 +11,10 @@ glob(`**/${node_modules_dir}`, {}, function (er, files) {
   // If the `nonull` option is set, and nothing
   // was found, then files is ["**/*.js"]
   // er is an error object or null.
-  console.log("files:::", files);
   for (const dir of files) {
-    console.log('fs.existsSync(dir):::', fs.existsSync(dir))
     if (fs.existsSync(dir)) {
+      console.log("removed:::", dir);
       rimraf.sync(dir);
     }
   }
 });
-// const regexp = pathToRegexp(node_modules_dir);
-// console.log("regexp", regexp);
-// rimraf(node_modules_dir, function (err) {
-//   // rimraf(regexp, function (err) {
-//   console.log(err);
-// });
